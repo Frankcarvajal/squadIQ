d61352d0eac82780699e3efa81e7ce3285d78112
HexisHacks <franklin.a.carvajal@gmail.com>
2017-03-09 16:18:30 -0500

    revisions

--- app.js
+++ app.js
@@ -21,22 +21,64 @@ Project: Capstone I (API)
 // ==========================
 
 // API Key assigned:
-const apiKey = "";
+const apiKey = "934d18fc6b4145d8bc4d19ac49de9dac";
 
 const initialState = {
     key: {}
 }
-
 
 // Step 3: CRUD Functions.
 // ==========================
 
 // Function to Obtain Data:
 // End Point:
-const url = "";
-// Define our query object with the given parameters and API key.
 
 
+// Independent variable to determine team data request
+i = 5;
+
+
+function getData() {
+    $.ajax({
+        headers: { 'X-Auth-Token': apiKey },
+        url: `http://api.football-data.org/v1/teams/${i}/players`,
+        dataType: 'json',
+        type: 'GET',
+    }).done(function(response) {
+        // console.log(response);
+
+        let names = response.players.map(function(item, index) {
+            return item.name;
+        });
+
+        let positions = response.players.map(function(item, index) {
+            return item.position;
+        });
+
+        let birthDates = response.players.map(function(item, index) {
+            return item.dateOfBirth;
+        });
+
+        let currentValues = response.players.map(function(item, index) {
+            return item.marketValue;
+        });
+
+        console.log(names);
+        console.log(positions);
+        console.log(birthDates);
+        console.log(currentValues);
+
+    });
+}
+getData();
+
+// iterate through each object in the array and pull out the specific data of the three variables.
+function makeData() {
+
+}
+// console.log(response.players[players.length].dateOfBirth);
+
+// response.players[0].dateOfBirth
 
 // Step 4: Render
 // ==========================
@@ -54,6 +96,6 @@ const url = "";
 
 //Invocation of Function with Arguments:
 
-$(function() {
+// $(function() {
 
-})
+// })
