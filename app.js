20d83804f73f6ceff0f933b633a0e285c44cab72
HexisHacks <franklin.a.carvajal@gmail.com>
2017-03-09 16:54:12 -0500

    revisions

--- app.js
+++ app.js
@@ -31,51 +31,61 @@ const initialState = {
 // ==========================
 
 // Function to Obtain Data:
-// End Point:
+
+
+function makeDataHtml(response) {
+    let mappedHtml = response.players.map(function(item, index) {
+        return item.name;
+
+    });
+    console.log(mappedHtml);
+    $('.datacoming').html(mappedHtml);
+}
 
 
 // Independent variable to determine team data request
 i = 5;
 
 
-function getData() {
+function getData(callback) {
     $.ajax({
         headers: { 'X-Auth-Token': apiKey },
         url: `http://api.football-data.org/v1/teams/${i}/players`,
         dataType: 'json',
         type: 'GET',
     }).done(function(response) {
+
+        return response;
+
         // console.log(response);
 
-        let names = response.players.map(function(item, index) {
-            return item.name;
-        });
+        // let names = response.players.map(function(item, index) {
+        //     return item.name;
+        // });
 
-        let positions = response.players.map(function(item, index) {
-            return item.position;
-        });
+        // let positions = response.players.map(function(item, index) {
+        //     return item.position;
+        // });
 
-        let birthDates = response.players.map(function(item, index) {
-            return item.dateOfBirth;
-        });
+        // let birthDates = response.players.map(function(item, index) {
+        //     return item.dateOfBirth;
+        // });
 
-        let currentValues = response.players.map(function(item, index) {
-            return item.marketValue;
-        });
+        // let currentValues = response.players.map(function(item, index) {
+        //     return item.marketValue;
+        // });
 
-        console.log(names);
-        console.log(positions);
-        console.log(birthDates);
-        console.log(currentValues);
+        // console.log(names);
+        // console.log(positions);
+        // console.log(birthDates);
+        // console.log(currentValues);
 
     });
 }
-getData();
+getData(makeDataHtml);
 
 // iterate through each object in the array and pull out the specific data of the three variables.
-function makeData() {
 
-}
 // console.log(response.players[players.length].dateOfBirth);
 
 // response.players[0].dateOfBirth
@@ -97,5 +107,41 @@ function makeData() {
 //Invocation of Function with Arguments:
 
 // $(function() {
+//     // Part of render function:
 
+//     const chartElement = $('#chart')
+
+//     new Chart(chartElement, {
+//         type: 'bubble',
+//         data: {
+//             datasets: [{
+//                 label: 'First Dataset',
+
+//                 // Data from state goes here:
+//                 data: [
+//                     { x: 10, y: 10, r: 10 },
+//                     { x: 15, y: 15, r: 10 }
+//                 ]
+//             }]
+//         },
+//         options: {
+//             scales: {
+//                 xAxes: [{
+//                     type: 'linear',
+//                     ticks: {
+//                         min: 0,
+//                         max: 20
+//                     }
+//                 }],
+
+//                 yAxes: [{
+//                     type: 'linear',
+//                     ticks: {
+//                         min: 0,
+//                         max: 20
+//                     }
+//                 }]
+//             }
+//         }
+//     })
 // })
