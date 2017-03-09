c882a8b3184aa7b3fdaf347fd5a0b1facf05f146
HexisHacks <franklin.a.carvajal@gmail.com>
2017-03-09 13:09:24 -0500

    Files added, revisions to index.html / CSS

---
+++ app.js
@@ -0,0 +1,59 @@
+/*
+Developers: Franklin Carvajal & Colin Van Sickle
+Date: 3.9.17
+Project: Capstone I (API)
+*/
+
+
+// Step 1: Requirements.
+// ==========================
+
+// 1. App should allow the user to search for an item (menu options clickable instead)
+// 2. List the results from the search (click from menu)
+// 3. Use API (must support CORS & JSONP is not an option)
+//    Should support https - required if deploying to hosts that use https. 
+//    And required if/when integrating into authentication
+
+// Bonus: integrate additional API like maps or form
+
+
+// Step 2: Design state & API Assigned
+// ==========================
+
+// API Key assigned:
+const apiKey = "";
+
+const initialState = {
+    key: {}
+}
+
+
+// Step 3: CRUD Functions.
+// ==========================
+
+// Function to Obtain Data:
+// End Point:
+const url = "";
+// Define our query object with the given parameters and API key.
+
+
+
+// Step 4: Render
+// ==========================
+
+
+
+
+// Step 5: User Actions
+// ==========================
+
+
+
+// Initialize
+// ==========================
+
+//Invocation of Function with Arguments:
+
+$(function() {
+
+})
